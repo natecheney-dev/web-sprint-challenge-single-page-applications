4a25d8030a577e7820bc8d81dbc5f4ef4a13bc2c
natecheney-dev <natecheney@gmail.com>
2021-09-17 18:58:46 -0500

    last changes

---
+++ cypress/integration/form_test_sepc.js
@@ -0,0 +1,27 @@
+describe("New User App", ()=>{
+    beforeEach(function (){
+        cy.visit('http://localhost:3000/pizza-form-container');
+    })
+
+    const name = () => cy.get('input[name=name]')
+    const button = () => cy.get('button')
+    const sauce = () => cy.get('input[name=sauce]')
+
+
+
+    describe('filling out inputs and button', () =>{
+        it('Checking For Inputs', ()=>{
+            name()
+                .should('have.value', '')
+                .type('Name input')
+                .should('have.value', 'Name input')
+        })
+        it('checking for non-check checkbox at start', () => {
+            sauce()
+                .should('not.be.checked')
+        })
+        it('submit button starts out disabled', ()=>{
+            button().should('be.disabled');
+        })
+    })
+})
